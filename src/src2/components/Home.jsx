c5e67ff639f9ba1435c62ad4cad5b06bc72f43dd
mahmud aremu <aremumahmud2003@gmail.com>
2024-05-28 19:23:40 +0100

    added second work

--- src/src2/components/Home.jsx
+++ src/src2/components/Home.jsx
@@ -14,7 +14,7 @@ import Why from "./why"
 function Home(){
     return(
         <> <Header />
-        <br /><br /><br /><br id="ff" />
+        <br /><br /><br className="ff" /><br className="ff" />
          <Hero />
          <CompanyValues /> 
         <div className="container">
